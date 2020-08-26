eb3990b1ace26ae42f757de45e788e9d5111825a
un4ckn0wl3z <slickerth@gmail.com>
2020-08-26 21:44:15 +0700

    update ui

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -5,6 +5,21 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+// Run every 2 sec
+setInterval(() => {
+  //
+  cpu.usage().then((info) => {
+    document.getElementById("cpu-usage").innerText = info + "%";
+  });
+
+  cpu.free().then((info) => {
+    document.getElementById("cpu-free").innerText = info + "%";
+  });
+
+  // uptime
+  document.getElementById("sys-uptime").innerText = secondsToDhms(os.uptime());
+}, 2000);
+
 // Set model
 document.getElementById("cpu-model").innerText = cpu.model();
 // Computer name
@@ -15,3 +30,13 @@ document.getElementById("os").innerText = `${os.type()} ${os.arch()}`;
 mem.info().then((info) => {
   document.getElementById("mem-total").innerText = info.totalMemMb;
 });
+
+// Show d/h/m/s
+function secondsToDhms(sec) {
+  sec = +sec;
+  const d = Math.floor(sec / (3600 * 24));
+  const h = Math.floor((sec % (3600 * 24)) / 3600);
+  const m = Math.floor((sec % 3600) / 60);
+  const s = Math.floor(sec % 60);
+  return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
