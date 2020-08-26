a7c03b890c305afacd8a7704e2c61cb9c9faa5eb
un4ckn0wl3z <slickerth@gmail.com>
2020-08-26 21:51:21 +0700

    calculate cpu progress

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -5,11 +5,19 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+let cpuOverload = 80;
+
 // Run every 2 sec
 setInterval(() => {
   //
   cpu.usage().then((info) => {
     document.getElementById("cpu-usage").innerText = info + "%";
+    document.getElementById("cpu-progress").style.width = info + "%";
+    if (info > cpuOverload) {
+      document.getElementById("cpu-progress").style.background = "red";
+    } else {
+      document.getElementById("cpu-progress").style.background = "#30c88b";
+    }
   });
 
   cpu.free().then((info) => {
